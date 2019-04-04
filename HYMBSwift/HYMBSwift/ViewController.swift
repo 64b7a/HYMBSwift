31dbd777d29ce74a04b94baf2e70d8aee28f04f1
MAC <>
2019-04-04 15:44:05 +0800

    swift工程初始化

---
+++ HYMBSwift/HYMBSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HYMBSwift
+//
+//  Created by 863Soft on 2019/4/4.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
